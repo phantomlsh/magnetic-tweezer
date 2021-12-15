e2bf016d58c820de86ea829f94e7b376237056d6
Phantomlsh <lishiheng2001@163.com>
2021-12-15 13:15:14 -0800

    Remove position

--- QI.py
+++ QI.py
@@ -8,12 +8,12 @@ def SamplePoints(N, Nθ, Nr):
 	θs = np.repeat(np.arange(π/Nθ, 2*π, 2*π/Nθ), Nr)
 	return rs * np.cos(θs), rs * np.sin(θs)
 
-def Quarters(Is, Nθ):
-	q = floor(Nθ/4)
-	return np.sum(Is[0:q], axis=0), np.sum(Is[q:2*q], axis=0), np.sum(Is[2*q:3*q], axis=0), np.sum(Is[3*q:4*q], axis=0)
-
 def XY(Is, Nθ):
-	Itr, Itl, Ibl, Ibr = Quarters(Is, Nθ)
+	q = floor(Nθ/4)
+	Itr = np.sum(Is[0:q], axis=0)
+	Itl = np.sum(Is[q:2*q], axis=0)
+	Ibl = np.sum(Is[2*q:3*q], axis=0)
+	Ibr = np.sum(Is[3*q:4*q], axis=0)
 	Il = (Itl + Ibl) / 2
 	Ir = (Itr + Ibr) / 2
 	It = (Itl + Itr) / 2
