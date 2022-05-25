2985d339abca80f1e6c5abc70a2044b91c6571d7
Phantomlsh <lishiheng2001@163.com>
2022-05-25 16:42:12 -0700

    Try taichi

--- kernel.py
+++ kernel.py
@@ -2,36 +2,35 @@ import taichi as ti
 import numpy as np
 ti.init(arch=ti.gpu)
 
+maxN = 100
 π = np.pi
+im = ti.field(dtype=ti.i32, shape=(1000, 1000))
+ps = ti.Vector.field(dtype=ti.f32, n=2, shape=(maxN))
+Is = ti.field(dtype=ti.f32, shape=(maxN, 100))
 
-def SetParams(r=40, nr=80, nθ=80):
-    global R, Nr, Nθ, Fr, Fθ
+def SetParams(H, W, r=40, nr=80, nθ=80):
+    global R, Nr, Nθ, Fr, Fθ, im, ps, Is
     R = r
     Nr = nr
     Nθ = nθ
     Fr = R/Nr
     Fθ = 2*π/Nθ
+    im = ti.field(dtype=ti.i32, shape=(H, W))
+    ps = ti.Vector.field(dtype=ti.f32, n=2, shape=(maxN))
+    Is = ti.field(dtype=ti.f32, shape=(maxN, Nr))
 
 @ti.kernel
-def tiBI(ims: ti.types.ndarray(), ps: ti.types.ndarray(), res: ti.types.ndarray()):
-    n = ps.shape[0]
-    ll = n * Nr # total result length
-    for l in range(ll):
-        r = l % Nr # index of ring
-        i = l // Nr # index of beads
+def tiBI(n: ti.i32):
+    for i, r in ti.ndrange(n, Nr):
+        Is[i, r] = 0
         for θ in range(Nθ):
-            x = ps[i, 0] + Fr * r * ti.cos(θ * Fθ) + R + 1
-            y = ps[i, 1] + Fr * r * ti.sin(θ * Fθ) + R + 1
+            x = ps[i][0] + Fr * r * ti.cos(θ * Fθ)
+            y = ps[i][0] + Fr * r * ti.sin(θ * Fθ)
 
             x0 = int(x)
             y0 = int(y)
             x1 = x0 + 1
             y1 = y0 + 1
-
-            Ia = ims[i, y0, x0]
-            Ib = ims[i, y1, x0]
-            Ic = ims[i, y0, x1]
-            Id = ims[i, y1, x1]
 
             xu = x1 - x
             xl = x - x0
@@ -43,22 +42,15 @@ def tiBI(ims: ti.types.ndarray(), ps: ti.types.ndarray(), res: ti.types.ndarray(
             wc = xl * yu
             wd = xl * yl
 
-            res[l] += wa*Ia + wb*Ib + wc*Ic + wd*Id
-
-        res[l] /= Nθ
+            Is[i, r] += (wa*im[y0, x0] + wb*im[y1, x0] + wc*im[y0, x1] + wd*im[y1, x1]) / Nθ
 
 def Profile(beads, img):
+    im.from_numpy(img.astype(int))
     n = len(beads)
-    ps = []
-    ims = []
-    for b in beads:
-        x0 = int(b.x)
-        y0 = int(b.y)
-        ims.append(img[y0-R-1:y0+R+1, x0-R-1:x0+R+1])
-        ps.append([b.x-x0, b.y-y0])
-    Is = np.zeros(Nr * n, dtype=np.float32)
-    tiBI(np.array(ims, dtype=np.float32), np.array(ps, dtype=np.float32), Is)
-    Is = Is.reshape((n, Nr))
     for i in range(n):
-        beads[i].profile = Is[i]
-    return Is
+        ps[i][0] = beads[i].x
+        ps[i][1] = beads[i].y
+    tiBI(n)
+    res = Is.to_numpy()
+    for i in range(n):
+        beads[i].profile = res[i]
