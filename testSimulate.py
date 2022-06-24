0352d872c5de59cb59db71b0ce703f83c5f0365e
Phantomlsh <phantomlsh@outlook.com>
2022-06-23 20:25:48 -0700

    Update testSimulate.py

--- testSimulate.py
+++ testSimulate.py
@@ -3,7 +3,7 @@ import taichi as ti
 import time
 import simulate
 import matplotlib.pyplot as plt
-import T
+import T as T
 
 img = np.ndarray((700, 756))
 simulate.Generate(50, 50, 50, 4, img)
@@ -12,11 +12,16 @@ beads = []
 for i in range(30):
     beads.append(T.Bead(50, 50))
 
-T.test(beads, img)
+T.XY(beads, img)
+
+T.Calibrate(beads, [img], 0)
+T.Calibrate(beads, [img], 1)
+T.Calibrate(beads, [img], 4)
+
+T.ComputeCalibration(beads)
 
 start = time.time()
 for i in range(1000):
-    T.test(beads, img)
+    T.XY(beads, img)
+    T.Z(beads, img)
 print("--- %s seconds ---" % (time.time() - start))
-
-print(beads[0].x)
