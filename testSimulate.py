560f8fa75fac5f1d4873204348ca9579bf6e553f
Phantomlsh <lishiheng2001@163.com>
2022-09-13 14:05:59 -0700

    N Stack

--- testSimulate.py
+++ testSimulate.py
@@ -14,53 +14,53 @@ for i in range(20):
 img = np.ndarray(shape)
 simulate.Generate(50, 50, 60, 4, img)
 
-maxn = 100
+maxn = 20
 loopRange = range(1, maxn)
-
-if (True):
-    ts = []
-    for loop in loopRange:
-        beads = []
-        for i in range(loop):
-            beads.append(T.Bead(50, 50))
-
-        T.XY(beads, img)
-
-        for i in range(20):
-            T.Calibrate(beads, [imgc[i]], i)
-
-        T.ComputeCalibration(beads)
-        T.XYZ(beads, img)
-        print(loop, beads[0])
-
-        start = time.time()
-        for i in range(100):
-            T.XYZ(beads, img)
-        ts.append(time.time() - start)
-
-    print(ts)
-    plt.plot(loopRange, np.array(ts), marker="o", label="GPU - Taichi")
-    print(np.polynomial.polynomial.polyfit(loopRange[1:], ts[1:], 1))
 
 if (False):
     ts = []
     for loop in loopRange:
         beads = []
         for i in range(loop):
+            beads.append(T.Bead(50, 50))
+
+        T.XY(beads, [img])
+
+        for i in range(20):
+            T.Calibrate(beads, [imgc[i]], i)
+
+        T.ComputeCalibration(beads)
+        T.XYZ(beads, [img])
+        print(loop, beads[0])
+
+        start = time.time()
+        for i in range(100):
+            T.XYZ(beads, [img])
+        ts.append(time.time() - start)
+
+    print(ts)
+    plt.plot(loopRange, np.array(ts), marker="o", label="GPU - Taichi")
+    print(np.polynomial.polynomial.polyfit(loopRange[1:], ts[1:], 1))
+
+if (True):
+    ts = []
+    for loop in loopRange:
+        beads = []
+        for i in range(loop):
             beads.append(N.Bead(50, 50))
 
-        N.XY(beads, img)
+        N.XY(beads, [img])
 
         for i in range(20):
             N.Calibrate(beads, [imgc[i]], i)
 
         N.ComputeCalibration(beads)
-        N.XYZ(beads, img)
+        N.XYZ(beads, [img])
         print(loop, beads[0])
 
         start = time.time()
         for i in range(100):
-            N.XYZ(beads, img)
+            N.XYZ(beads, [img])
         ts.append(time.time() - start)
 
     print(ts)
